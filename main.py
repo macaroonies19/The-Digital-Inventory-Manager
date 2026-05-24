28dc1f957e5fe8ec577dc6777a28571e38c49a34
macaroonies19 <alexalksnis@gmail.com>
2026-05-25 09:58:39 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+class Product:
+  def __init__(self, product_id, name, price, quantity):
+    self.product_id = product_id
+    self.name = name
+    self.price = price
+    self.quantity = quantity
 
+  def update_stock(self, amount):
+    self.amount = amount
+    if amount = 
+
+  def get_total_value():
+    
+
+  def __str__(self):
+    return f"{self.product_id} {self.name} {self.price} {self.quantity}"
